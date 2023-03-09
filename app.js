c1ce778684d2786d07125080ce617e82265fec6b
Ramazan Mert Kaya <kmert0@outlook.com>
2023-03-09 22:21:52 +0300

    add about & contact page

--- app.js
+++ app.js
@@ -18,13 +18,21 @@ app.set("view engine", "ejs");
 
 app.set("views", "./views");
 app.use(express.static('public'));
-app.set('views', './views');
+app.set('partials', './views/partials');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home",{startingContent: homeStartingContent});
+  res.render("home",{homeStartingContent: homeStartingContent});
+});
+
+app.get('/about', function(req, res) {
+  res.render('about', { aboutContent:  aboutContent});
+});
+
+app.get('/contact', function(req, res) {
+  res.render('contact', { contactContent:  contactContent});
 });
 
 app.listen(3000, function () {
